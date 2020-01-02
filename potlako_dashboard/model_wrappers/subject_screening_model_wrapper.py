85ccd8511754134a75d996af673ede2b66af578e
ckgathi <ckgathi@gmail.com>
2020-01-02 18:26:46 +0200

    fix listboards

--- potlako_dashboard/model_wrappers/subject_screening_model_wrapper.py
+++ potlako_dashboard/model_wrappers/subject_screening_model_wrapper.py
@@ -4,7 +4,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from edc_model_wrapper import ModelWrapper
 
 from edc_consent import ConsentModelWrapperMixin
-from edc_consent.site_consents import site_consents
 
 from .subject_consent_model_wrapper import SubjectConsentModelWrapper
 
@@ -12,34 +11,14 @@ from .subject_consent_model_wrapper import SubjectConsentModelWrapper
 class SubjectScreeningModelWrapper(
         ConsentModelWrapperMixin,
         ModelWrapper):
-
+    
     consent_model_wrapper_cls = SubjectConsentModelWrapper
     model = 'potlako_subject.subjectscreening'
-    next_url_attrs = ['screening_identifier', 'subject_identifier']
-    next_url_name = settings.DASHBOARD_URL_NAMES.get(
-        'screening_listboard_url')
-
-    @property
-    def consent_object(self):
-        """Returns a consent configuration object from site_consents
-        relative to the wrapper's "object" report_datetime.
-        """
-        default_consent_group = django_apps.get_app_config(
-            'edc_consent').default_consent_group
-        consent_object = site_consents.get_consent_for_period(
-            model=self.consent_model_wrapper_cls.model,
-            report_datetime=self.consent_version_model_obj.report_datetime,
-            consent_group=default_consent_group,
-            version=self.consent_version or None)
-
-        return consent_object
+    next_url_attrs = ['screening_identifier']
+    next_url_name = settings.DASHBOARD_URL_NAMES.get('screening_listboard_url')
 
     @property
     def consented(self):
-        return self.object.subject_identifier
-
-    @property
-    def subject_identifier(self):
         return self.object.subject_identifier
 
     @property
@@ -47,6 +26,10 @@ class SubjectScreeningModelWrapper(
         options = super().create_consent_options
         options.update(screening_identifier=self.object.screening_identifier)
         return options
+
+    @property
+    def consent_options(self):
+        return dict(screening_identifier=self.object.screening_identifier)
 
     @property
     def consent_model_obj(self):
--- potlako_dashboard/templatetags/potlako_dashboard_extras.py
+++ potlako_dashboard/templatetags/potlako_dashboard_extras.py
@@ -3,6 +3,38 @@ from django.conf import settings
 
 register = template.Library()
 
+@register.inclusion_tag('potlako_dashboard/buttons/screening_button.html')
+def screening_button(model_wrapper):
+    title = ['Edit subject\' screening form.']
+    return dict(
+        screening_identifier=model_wrapper.object.screening_identifier,
+        href=model_wrapper.href,
+        title=' '.join(title))
+
+
+@register.inclusion_tag('potlako_dashboard/buttons/eligibility_button.html')
+def eligibility_button(subject_screening_model_wrapper):
+    comment = []
+    obj = subject_screening_model_wrapper.object
+    tooltip = None
+    if not obj.is_eligible:
+        comment = obj.ineligibility.split(',')
+    comment = list(set(comment))
+    comment.sort()
+    return dict(eligible=obj.is_eligible, comment=comment,
+                tooltip=tooltip, obj=obj)
+
+
+@register.inclusion_tag('potlako_dashboard/buttons/consent_button.html')
+def consent_button(model_wrapper):
+    title = ['Consent subject to participate.']
+    consent_version = model_wrapper.consent.version
+    return dict(
+        screening_identifier=model_wrapper.object.screening_identifier,
+        add_consent_href=model_wrapper.consent.href,
+        consent_version=consent_version,
+        title=' '.join(title))
+
 
 @register.inclusion_tag('potlako_dashboard/buttons/dashboard_button.html')
 def dashboard_button(model_wrapper):
