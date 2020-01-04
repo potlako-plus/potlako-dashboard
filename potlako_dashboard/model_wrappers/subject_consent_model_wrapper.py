473f38e7ce71e9d1d5f6420bb71c8f9599ff1f36
ckgathi <ckgathi@gmail.com>
2020-01-04 21:13:39 +0200

    fix consent buttons

--- potlako_dashboard/model_wrappers/subject_consent_model_wrapper.py
+++ potlako_dashboard/model_wrappers/subject_consent_model_wrapper.py
@@ -5,6 +5,6 @@ from edc_model_wrapper import ModelWrapper
 class SubjectConsentModelWrapper(ModelWrapper):
 
     model = 'potlako_subject.subjectconsent'
-    next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_listboard_url')
-    next_url_attrs = ['subject_identifier']
-    querystring_attrs = ['gender', 'first_name', 'initials', 'modified']
+    next_url_name = settings.DASHBOARD_URL_NAMES.get('screening_listboard_url')
+    next_url_attrs = ['screening_identifier']
+    querystring_attrs = ['screening_identifier', 'subject_identifier']
