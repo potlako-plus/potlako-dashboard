fa95ebe711b6f9dbfbe698213902a5a28411cdde
COulson Kgathi <ckgathi@gmail.com>
2020-03-06 11:32:24 +0200

    fix appointment mode wrapper

--- potlako_dashboard/model_wrappers/subject_visit_model_wrapper.py
+++ potlako_dashboard/model_wrappers/subject_visit_model_wrapper.py
@@ -7,8 +7,4 @@ class SubjectVisitModelWrapper(BaseSubjectVisitModelWrapper):
     model = 'potlako_subject.subjectvisit'
     next_url_name = settings.DASHBOARD_URL_NAMES.get(
         'subject_dashboard_url')
-    next_url_attrs = ['subject_identifier', 'appointment', 'reason']
-
-    @property
-    def appointment(self):
-        return str(self.object.appointment.id)
+    next_url_attrs = ['subject_identifier', 'appointment']
